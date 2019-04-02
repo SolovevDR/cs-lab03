5827e7408502d3b984c9f6a06eee043e6deba54d
lab03 (SolovevDR) <rediska047@mail.ru>
2019-04-02 14:46:36 +0300

    колличество столбцов через гистограмму

--- main.cpp
+++ main.cpp
@@ -26,6 +26,24 @@ find_minmax(vector<double> numbers, double& min, double& max) {
     }
 }
 
+vector <size_t>
+make_histogram( vector<double> numbers, size_t bin_count)
+{
+    double min ;
+    double max ;
+    find_minmax(numbers,min,max);
+
+    vector<size_t> bins(bin_count);
+    for (double number : numbers) {
+        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
+        if (bin == bin_count) {
+            bin--;
+        }
+        bins[bin]++;
+    }
+    return bins;
+}
+
 int
 main() {
     // Ввод данных
@@ -41,18 +59,8 @@ main() {
     cin >> bin_count;
 
     // Обработка данных
-    double min ;
-    double max ;
-    find_minmax(numbers,min,max);
 
-    vector<size_t> bins(bin_count);
-    for (double number : numbers) {
-        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-        if (bin == bin_count) {
-            bin--;
-        }
-        bins[bin]++;
-    }
+    vector <size_t> bins = make_histogram(numbers, bin_count);
 
     // Вывод данных
     const size_t SCREEN_WIDTH = 80;
