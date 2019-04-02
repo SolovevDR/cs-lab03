e9317e4fa510eb58edff234ac580ae3f44dec8aa
lab03 (SolovevDR) <rediska047@mail.ru>
2019-04-02 17:06:06 +0300

    тесты

---
+++ lab03-test/main.cpp
@@ -0,0 +1,47 @@
+#include "../histogram.h"
+
+#include <cassert>
+
+void
+test_positive() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 2, 3}, min, max);
+    assert(min == 1);
+    assert(max == 3);
+}
+
+void
+test_negativ() {
+    double min = 0;
+    double max = 0;
+    find_minmax({-1, -2, -3}, min, max);
+    assert(min == -3);
+    assert(max == -1);
+}
+
+void
+test_odinakov() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 1, 1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+}
+
+void
+test_one() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+}
+
+int main()
+{
+       test_positive();
+       test_negativ();
+       test_odinakov();
+       test_one();
+}
